4ef45bd875889b87f91ccceabd33c7fe8a8cd06b
sfhmi <s.fahmii29@gmail.com>
2023-02-02 22:56:43 +0700

    first commit

---
+++ src/utils/hello.ts
@@ -0,0 +1,11 @@
+/**
+ * Provide a name, to say hello to.
+ *
+ * @param {string} name
+ * @returns {string}!
+ */
+export const hello = (name: string) => {
+    const hello = "Hello, ";
+    const message = `${hello}${name}!`;
+    return message;
+};
