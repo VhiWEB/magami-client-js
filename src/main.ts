d10d59fa3966ecf2b8ada8e41e29b07a81ffa298
sfhmi <s.fahmii29@gmail.com>
2023-02-09 01:52:51 +0700

    chore: create class and method for  prototype function

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,45 @@
+import { api } from "./helpers/api";
 import { hello } from "./utils/hello";
 
 export { hello }
+
+// interface Coupon {
+//     data?: any,
+// }
+
+interface UserData {
+    name?: string,
+    id?: number,
+    phone?: number,
+    address?: string,
+    email?: string,
+}
+
+export default class Magami {
+    // private static _instance = Magami;
+
+    async claimCupoun(couponCode: string | number) {
+        try {
+            const response = api('POST', `${couponCode}`)
+            if (response) {
+                return (await response).json()
+            }
+        } catch (error) {
+            throw new Error;
+        }
+    }
+
+    async play(payload: UserData) {
+        try {
+            const response = api('POST', `/`, {
+                payload,
+            })
+            if (response) {
+                return (await response).json()
+            }
+        } catch (error) {
+            throw new Error;
+        }
+    }
+
+}
