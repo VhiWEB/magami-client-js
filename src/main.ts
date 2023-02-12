bceaae705ab63377b5027a03917c5604187ec623
sfhmi <s.fahmii29@gmail.com>
2023-02-12 14:00:06 +0700

    chore: create storage data, update private data, and create api call  handler for request

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,54 @@
-import { coreApi } from "./helpers/api";
-import { } from "./utils/storage";
+import { coreApi } from "./helpers/api"
+import { storeDataManagement } from "./utils/storage"
 
 interface Init {
     appId?: string | undefined | any
-    userName?: string | undefined | any
     uniqueKey?: string | undefined | any
 }
 
 export default class Magami {
 
-    init({ appId, userName, uniqueKey }: Init): void {
-        localStorage.setItem('appId', appId)
-        localStorage.setItem('userName', userName)
-        localStorage.setItem('uniqueKey', uniqueKey)
+    private storage = storeDataManagement()
+
+    private get appId() {
+        return this.storage.getAppId()
+    }
+
+    private get uniqueKey() {
+        return this.storage.getUniqueKey()
+    }
+
+    init({ appId, uniqueKey }: Init): void {
+        this.storage.setAppId(appId)
+        this.storage.setUniqueKey(uniqueKey)
+    }
+
+    private async apiCall(method: string, resource: string, body?: Record<string, unknown>) {
+        // setup authorization for headers
+        const encoded = btoa(`${this.appId}:${this.uniqueKey}`);
+        const auth = `Basic ${encoded}`
+
+        try {
+            const response = await coreApi(method, resource, auth, body)
+            if (response) {
+                const data = await response.json()
+                return data;
+            }
+        } catch (error) {
+            return error
+        }
     }
 
     async claim(couponCode: string) {
         try {
-            const response = await coreApi('GET', `/${couponCode}`)
+            const response = await this.apiCall('GET', `claim/${couponCode}`);
             if (response) {
-                return await response.json()
+                return response;
+            } else {
+                throw new Error;
             }
         } catch (error) {
-            throw new Error()
+            return error;
         }
     }
 }
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,5 +1,24 @@
-export const storage = {
-    appId() {
-        return localStorage.getItem('appId')
+
+
+export const storeDataManagement = () => {
+    const store: Record<string, any> = {}
+
+    const Setter = <T>(name: string) => {
+        return (value: T): void => {
+            store[name] = value
+        }
     }
+
+    const Getter = <T>(name: string, defaultVal?: T) => {
+        return (): T => {
+            return store[name] ?? defaultVal
+        }
+    }
+
+    return ({
+        getAppId: Getter<string>('appId'),
+        setAppId: Setter<string>('appId'),
+        getUniqueKey: Getter<string>('uniqueKey'),
+        setUniqueKey: Setter<string>('uniqueKey'),
+    })
 }
