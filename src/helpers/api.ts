7ecdf53fd80f2a508f9490a6a6450ae8a2a0604f
sfhmi <s.fahmii29@gmail.com>
2023-02-12 13:59:05 +0700

    fix: unauthorize api call

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,12 +1,11 @@
-import { storage } from "../utils/storage";
 
-export const coreApi = (method?: string, resource?: string, data?: Record<string, unknown>) => {
-    const baseURL = 'magami'
+export const coreApi = (method?: string, resource?: string, authKey?: string, data?: Record<string, unknown>) => {
+    const baseURL = 'http://bihunku-be.embrio.id/api'
     return fetch(`${baseURL}/${resource}`, {
         method,
         headers: {
             'content-type': 'application/json',
-            'appId': `${storage.appId}`,
+            'Authorization': `${authKey}`,
         },
         body: data && JSON.stringify(data)
     });
