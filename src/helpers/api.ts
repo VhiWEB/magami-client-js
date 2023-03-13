a61b6888bb787e3cc411444ca30fdecc79faac20
sfhmi <s.fahmii29@gmail.com>
2023-03-13 19:31:09 +0700

    chore: changing api

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,7 +1,13 @@
 
 export const coreApi = (method?: string, resource?: string, auth?: string, slug?: string, data?: Record<string, unknown>,) => {
 
-    let baseURL: any = new URL(`http://app.magami.id/api/v1/campaigns/${slug}/${resource}`)
+    //prod api
+    // let baseURL: any = new URL(`http://app.magami.id/api/v1/campaigns/${slug}/${resource}`)
+
+    //stg api
+    let baseURL: any = new URL(`https://demo.magami.id/api/v1/campaigns/${slug}/${resource}`)
+
+
 
     return fetch(`${baseURL}`, {
         method,
