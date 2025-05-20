c8882d8ec9d41aaf9d9e754d76d192da738750fc
rhnnss <muhrayhanns04@gmail.com>
2025-05-20 09:28:22 +0700

    feat: add rewards API call and update package version
    Add new `rewards` method to fetch rewards list in the Magami client. Update package version to 2.3.4 and add new contributor to package.json. Also, update CHANGELOG.md to reflect the new feature.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,91 +1,115 @@
 # Changelog
 
+## v.2.3.3 - [2025-03-13]
+
+### added
+
+-   adding new api call request to get list rewards
+
 ## v2.3.2 - [2023-02-12]
+
 ### patch
-- path versioning that cause new update are not installed
+
+-   path versioning that cause new update are not installed
+
 ## v2.3.1 - [2023-02-12]
+
 ### patch
-- new payload on winner form
+
+-   new payload on winner form
 
 ## v2.3.0 - [2023-12-26]
+
 ### added
-- adding new api call request to get campaign information such as theme color, banner, logo, and etc
-- adding new api call request to validate token signature
+
+-   adding new api call request to get campaign information such as theme color, banner, logo, and etc
+-   adding new api call request to validate token signature
 
 ## v2.2.5 - [2023-11-35]
+
 ### Patch
-- removing unused item
+
+-   removing unused item
 
 ## v2.2.4 - [2023-08-13]
+
 ### Patch
-- core api refactoring 
-- data request update
+
+-   core api refactoring
+-   data request update
 
 ## v2.2.3 - [2023-04-13]
+
 ### Patch
-- adding new variable to winner form payload
+
+-   adding new variable to winner form payload
 
 ## v2.2.2 - [2023-04-10]
+
 ### fix
-- fix leaderboard endpoint
+
+-   fix leaderboard endpoint
 
 ## v2.2.1 - [2023-04-07]
+
 ### Added
-- add new feature for get leaderboard
+
+-   add new feature for get leaderboard
 
 ## v2.2.0 - [2023-04-03]
+
 ### Patch
-- add new feature for get locations
-- add new api call for basic api call with no campaign slug
-- update coreApi and apiCall function
+
+-   add new feature for get locations
+-   add new api call for basic api call with no campaign slug
+-   update coreApi and apiCall function
 
 ## v2.1.3 - [2023-03-16]
+
 ### Patch
-- Fixing wrong endpoints on every request API call
+
+-   Fixing wrong endpoints on every request API call
 
 ## v2.1.2 - [2023-03-15]
+
 ### Minor Updates
-- update apiCall function
-- update coreApi on helper
-- add protected api url so it can change dynamicly from user
+
+-   update apiCall function
+-   update coreApi on helper
+-   add protected api url so it can change dynamicly from user
 
 ## v2.0.2 - [2023-03-13]
+
 ### Patch
-- wrong end point
+
+-   wrong end point
 
 ## v2.0.1 - [2023-03-13]
+
 ### Patch
-- forgot to build the app -___-
+
+-   forgot to build the app -\_\_\_-
 
 ## v2.0.0 - [2023-03-13]
+
 ### Major
-- changing api address
+
+-   changing api address
 
 ## v1.0.1 - [2023-03-10]
+
 ### Patch
-- fix wrong type object implementation for all functions
+
+-   fix wrong type object implementation for all functions
 
 ## v1.0.0 - [2023-02-20]
+
 ### Added
-- Initialization function to run Magami
-- Add storage setter and getter to store data from initialization
-- Add module interface to store all typescript interface from storage data or from anywhere else
-- Add set user data method and request
-- Add few methods welcomeForm, redeem, validateWinner, winnerForm, getWinner, and faq
-- Add API configuration method.
-- Add API call method to sent request to api
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+-   Initialization function to run Magami
+-   Add storage setter and getter to store data from initialization
+-   Add module interface to store all typescript interface from storage data or from anywhere else
+-   Add set user data method and request
+-   Add few methods welcomeForm, redeem, validateWinner, winnerForm, getWinner, and faq
+-   Add API configuration method.
+-   Add API call method to sent request to api
--- src/main.ts
+++ src/main.ts
@@ -1,282 +1,349 @@
-import { coreApi, coreApiBasicAuth, coreApiNoSlug } from "./helpers/api"
-import { storeDataManagement } from "./utils/storage"
-import * as model from "./utils/model"
+import { coreApi, coreApiBasicAuth, coreApiNoSlug } from "./helpers/api";
+import { storeDataManagement } from "./utils/storage";
+import * as model from "./utils/model";
 
 export default class Magami {
+	private storage = storeDataManagement();
 
-    private storage = storeDataManagement()
+	protected apiURL = `https://app.magami.id/api/v1`;
 
-    protected apiURL = `https://app.magami.id/api/v1`
+	private get apiKey() {
+		return this.storage.getApiKey();
+	}
 
-    private get apiKey() {
-        return this.storage.getApiKey()
-    }
+	private get campaignSlug() {
+		return this.storage.getCampaignSlug();
+	}
 
-    private get campaignSlug() {
-        return this.storage.getCampaignSlug()
-    }
+	init({ apiKey, campaignSlug }: model.Init): void {
+		this.storage.setApiKey(apiKey);
+		this.storage.setCampaignSlug(campaignSlug);
+	}
 
-    init({ apiKey, campaignSlug }: model.Init): void {
-        this.storage.setApiKey(apiKey)
-        this.storage.setCampaignSlug(campaignSlug)
-    }
+	// setup authorization for headers
+	private async apiCall(
+		method: string,
+		resource: string,
+		body?: Record<string, unknown>
+	): Promise<any> {
+		const auth = this.apiKey;
+		const slug = this.campaignSlug;
+		try {
+			const response = await coreApi(
+				method,
+				resource,
+				auth,
+				slug,
+				body,
+				this.apiURL
+			);
+			if (response) {
+				return response;
+			}
+		} catch (error: any) {
+			throw Error(error);
+		}
+	}
+	private async apiCalNoSlug(
+		method: string,
+		resource: string,
+		body?: Record<string, unknown>
+	) {
+		const auth = this.apiKey;
+		try {
+			const response = await coreApiNoSlug(
+				method,
+				resource,
+				auth,
+				body,
+				this.apiURL
+			);
+			if (response) {
+				const data = await response.json();
+				return data;
+			}
+		} catch (error: any) {
+			throw Error(error);
+		}
+	}
 
-    // setup authorization for headers
-    private async apiCall(method: string, resource: string, body?: Record<string, unknown>): Promise<any> {
-        const auth = this.apiKey;
-        const slug = this.campaignSlug;
-        try {
-            const response = await coreApi(method, resource, auth, slug, body, this.apiURL)
-            if (response) {
-                return response;
-            }
-        } catch (error: any) {
-            throw Error(error)
-        }
-    }
-    private async apiCalNoSlug(method: string, resource: string, body?: Record<string, unknown>) {
-        const auth = this.apiKey;
-        try {
-            const response = await coreApiNoSlug(method, resource, auth, body, this.apiURL)
-            if (response) {
-                const data = await response.json()
-                return data;
-            }
-        } catch (error: any) {
-            throw Error(error)
+	private async apiCallBasic(
+		method: string,
+		resource: string,
+		username: string,
+		password: string,
+		body?: Record<string, unknown>
+	) {
+		try {
+			const response = await coreApiBasicAuth(
+				method,
+				resource,
+				body,
+				username,
+				password,
+				this.apiURL
+			);
+			if (response) {
+				const data = await response.json();
+				return data;
+			}
+		} catch (error: any) {
+			throw Error(error);
+		}
+	}
 
-        }
-    }
+	async claim(couponCode: string) {
+		try {
+			const response = await this.apiCall("GET", `check/${couponCode}`);
+			if (response) {
+				this.storage.setCouponCode(couponCode);
+				return response;
+			} else {
+				throw new Error();
+			}
+		} catch (error) {
+			return error;
+		}
+	}
 
-    private async apiCallBasic(method: string, resource: string, username: string, password: string, body?: Record<string, unknown>) {
-        try {
-            const response = await coreApiBasicAuth(method, resource, body, username, password, this.apiURL)
-            if (response) {
-                const data = await response.json()
-                return data;
-            }
-        } catch (error: any) {
-            throw Error(error)
+	async welcomeForm({
+		coupon_code,
+		name,
+		phone,
+		province_id,
+		city_id,
+		district_id,
+	}: {
+		coupon_code: string;
+		name: string;
+		phone: string | number;
+		province_id: string | number;
+		city_id: string | number;
+		district_id: string | number;
+	}) {
+		const payload = {
+			name,
+			phone,
+			province_id,
+			city_id,
+			district_id,
+		};
+		try {
+			const response: any = await this.apiCall(
+				"POST",
+				`welcome/submit/${coupon_code}`,
+				{
+					...payload,
+				}
+			);
 
-        }
-    }
+			if (response) {
+				return response;
+			} else {
+				throw new Error();
+			}
+		} catch (error) {
+			return error;
+		}
+	}
 
-    async claim(couponCode: string) {
-        try {
-            const response = await this.apiCall('GET', `check/${couponCode}`);
-            if (response) {
-                this.storage.setCouponCode(couponCode)
-                return response;
-            } else {
-                throw new Error;
-            }
-        } catch (error) {
-            return error;
-        }
-    }
+	async redeem(redemptionId: string) {
+		try {
+			const response = await this.apiCall(
+				"GET",
+				`redeem/${redemptionId}`
+			);
 
-    async welcomeForm({
-        coupon_code,
-        name,
-        phone,
-        province_id,
-        city_id,
-        district_id,
-    }: {
-        coupon_code: string,
-        name: string,
-        phone: string | number,
-        province_id: string | number,
-        city_id: string | number,
-        district_id: string | number,
-    }) {
-        const payload = {
-            name,
-            phone,
-            province_id,
-            city_id,
-            district_id
-        }
-        try {
-            const response: any = await this.apiCall('POST', `welcome/submit/${coupon_code}`, {
-                ...payload
-            })
+			if (response) {
+				return response;
+			} else {
+				throw new Error();
+			}
+		} catch (error) {
+			return error;
+		}
+	}
 
-            if (response) {
-                return response
-            } else {
-                throw new Error
-            }
-        } catch (error) {
-            return error
-        }
-    }
+	async rewards() {
+		try {
+			const response = await this.apiCall("GET", `rewards`);
 
-    async redeem(redemptionId: string) {
-        try {
-            const response = await this.apiCall('GET', `redeem/${redemptionId}`)
+			if (response) {
+				return response;
+			} else {
+				throw new Error();
+			}
+		} catch (error) {
+			return error;
+		}
+	}
 
-            if (response) {
-                return response
-            } else {
-                throw new Error;
+	async validateWinner({
+		coupon_code,
+		phone,
+	}: {
+		coupon_code: string;
+		phone: string | number;
+	}) {
+		try {
+			const response = await this.apiCall("POST", `winner/validate`, {
+				coupon_code,
+				phone,
+			});
 
-            }
+			if (response) {
+				return response;
+			}
+		} catch (error) {
+			return error;
+		}
+	}
 
-        } catch (error) {
-            return error
-        }
-    }
+	async winnerForm({
+		redemption_id,
+		email,
+		id_number,
+		address,
+		...additionalProps
+	}: {
+		redemption_id: string | number;
+		email: string;
+		id_number: string | number;
+		address?: string;
+		[key: string]: any;
+	}): Promise<any> {
+		try {
+			const response = await this.apiCall(
+				"POST",
+				`winner/submit/${redemption_id}`,
+				{
+					email,
+					id_number,
+					address,
+					...additionalProps,
+				}
+			);
 
-    async validateWinner({ coupon_code, phone }: {
-        coupon_code: string,
-        phone: string | number,
-    }) {
-        try {
-            const response = await this.apiCall('POST', `winner/validate`, {
-                coupon_code,
-                phone,
-            })
+			if (response) {
+				return response;
+			}
+		} catch (error) {
+			return error;
+		}
+	}
 
-            if (response) {
-                return response
-            }
-        } catch (error) {
-            return error
-        }
-    }
+	async getWinner(): Promise<any> {
+		try {
+			const response = await this.apiCall("GET", `winner/list`);
 
-    async winnerForm({ redemption_id, email, id_number, address, zip_code, province_id, district_id, city_id }: {
-        redemption_id: string | number
-        email: string
-        id_number: string | number
-        address: string
-        zip_code: string | number
-        province_id: string | number
-        district_id: string | number
-        city_id: string | number
-    }): Promise<any> {
-        try {
-            const response = await this.apiCall('POST', `winner/submit/${redemption_id}`, {
-                email,
-                id_number,
-                address,
-                zip_code,
-                province_id,
-                district_id,
-                city_id,
-            })
+			if (response) {
+				return response;
+			}
+		} catch (error) {
+			return error;
+		}
+	}
 
-            if (response) {
-                return response
-            }
+	async faq(searchKey?: string | number | any): Promise<any> {
+		try {
+			const reqUrl = `faq`;
 
-        } catch (error) {
-            return error
-        }
-    }
+			searchKey && reqUrl.concat(`?search=${searchKey}`);
 
-    async getWinner(): Promise<any> {
-        try {
+			const response = await this.apiCall("POST", `${reqUrl}`);
 
-            const response = await this.apiCall('GET', `winner/list`,)
+			if (response) {
+				return response;
+			}
+		} catch (error) {
+			return error;
+		}
+	}
 
-            if (response) {
-                return response
-            }
+	async getProvince(): Promise<any> {
+		try {
+			const response = await this.apiCalNoSlug(
+				"GET",
+				"locations/provinces"
+			);
 
-        } catch (error) {
-            return error
-        }
-    }
+			if (response) {
+				return response;
+			}
+		} catch (error) {
+			return error;
+		}
+	}
 
-    async faq(searchKey?: string | number | any): Promise<any> {
-        try {
-            const reqUrl = `faq`
+	async getCities(provinceId: number | string): Promise<any> {
+		try {
+			const response = await this.apiCalNoSlug(
+				"GET",
+				`locations/cities/${provinceId}`
+			);
 
-            searchKey && reqUrl.concat(`?search=${searchKey}`)
+			if (response) {
+				return response;
+			}
+		} catch (error) {
+			return error;
+		}
+	}
 
-            const response = await this.apiCall('POST', `${reqUrl}`)
+	async getDistrict(cityId: number | string): Promise<any> {
+		try {
+			const response = await this.apiCalNoSlug(
+				"GET",
+				`locations/districts/${cityId}`
+			);
 
-            if (response) {
-                return response
-            }
+			if (response) {
+				return response;
+			}
+		} catch (error) {
+			return error;
+		}
+	}
 
-        } catch (error) {
-            return error
-        }
-    }
+	async getLeaderboard(type: string) {
+		try {
+			const response = await this.apiCall("GET", `leaderboard/${type}`);
 
-    async getProvince(): Promise<any> {
-        try {
-            const response = await this.apiCalNoSlug('GET', 'locations/provinces')
+			if (response) {
+				return response;
+			}
+		} catch (error) {
+			return error;
+		}
+	}
 
-            if (response) {
-                return response
-            }
+	async getConfiguration(host: string, username: string, password: string) {
+		try {
+			const response = await this.apiCallBasic(
+				"GET",
+				`tenant/${host}`,
+				username,
+				password
+			);
+			if (response) {
+				return response;
+			}
+		} catch (error) {
+			return error;
+		}
+	}
 
-        } catch (error) {
-            return error
-        }
-    }
-
-    async getCities(provinceId: number | string): Promise<any> {
-        try {
-            const response = await this.apiCalNoSlug('GET', `locations/cities/${provinceId}`)
-
-            if (response) {
-                return response
-            }
-        } catch (error) {
-            return error
-        }
-    }
-
-    async getDistrict(cityId: number | string): Promise<any> {
-        try {
-            const response = await this.apiCalNoSlug('GET', `locations/districts/${cityId}`)
-
-            if (response) {
-                return response
-            }
-        } catch (error) {
-            return error
-        }
-    }
-
-    async getLeaderboard(type: string) {
-        try {
-            const response = await this.apiCall('GET', `leaderboard/${type}`)
-
-            if (response) {
-                return response
-            }
-        } catch (error) {
-            return error
-        }
-    }
-
-    async getConfiguration(host: string, username: string, password: string) {
-        try {
-            const response = await this.apiCallBasic('GET', `tenant/${host}`, username, password)
-            if (response) {
-                return response
-            }
-        } catch (error) {
-            return error
-        }
-    }
-
-
-
-    async validateSignature(signature: string) {
-        try {
-            const request: any = await this.apiCall('GET', `session/validate/${signature}`)
-            if (request) {
-                return request
-            }
-        } catch (error: any) {
-            return error
-        }
-    }
+	async validateSignature(signature: string) {
+		try {
+			const request: any = await this.apiCall(
+				"GET",
+				`session/validate/${signature}`
+			);
+			if (request) {
+				return request;
+			}
+		} catch (error: any) {
+			return error;
+		}
+	}
 }
